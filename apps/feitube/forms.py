a588dd966c634e41555c97b21306b60535df878b
diegomatus <cube.di.rubik@gmail.com>
2015-11-30 14:08:04 -0600

    score videos

--- apps/feitube/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class VideoForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    tags = forms.CharField(max_length=255)
-    description = forms.CharField(max_length=1024)
-    file = forms.FileField()
